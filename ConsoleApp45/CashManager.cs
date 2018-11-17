824d63935e867849c90bab56180051588b37370a
Maikel <Maikelsitbon@gmail.com>
2018-11-18 01:05:18 +0200

    Add project files.

---
+++ ConsoleApp45/CashManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    public class CashManager
+    {
+        private int StrongCount;
+        public Dictionary<int,object> Strong { get; set; }
+
+        private int WeakCount;
+        //static public Dictionary<int, object> Weak { get; set; }
+        WeakReference Weak { get; set; }
+
+        public CashManager(int MaxStrong,int MaxWeak)
+        {
+            StrongCount = MaxStrong;
+            WeakCount = MaxWeak;
+            Weak = new WeakReference(new Dictionary<int, object>());
+            Strong = new Dictionary<int, object>();
+        }
+
+        public bool Add(int id,object obj)
+        {
+            if (StrongCount > 0)
+            {
+                Strong.Add(id, obj);
+                StrongCount--;
+                return true;
+            }
+            else if (WeakCount > 0)
+            {
+                Dictionary<int, object> DicTmp = (Dictionary<int, object>)Weak.Target;
+                DicTmp.Add(id, obj);
+                Weak = new WeakReference(DicTmp);
+                WeakCount--;
+                return true;
+            }
+            return false;
+        }
+
+        public bool GetAndRemove(int id, out object obj)
+        {
+            obj = null;
+            object tmp = null;
+            if (Strong.FirstOrDefault(c=>c.Key == id).Value != null) tmp = Strong[id];
+            
+
+            if (tmp == null)
+            {
+                Dictionary<int, object> DicTmp = (Dictionary<int, object>)Weak.Target;
+                if (DicTmp == null) return false;
+                object tmp2 = null;
+                if (DicTmp.FirstOrDefault(c => c.Key == id).Value != null) tmp2 = DicTmp[id];
+
+                if (tmp2 == null) return false;
+                else
+                {
+                    obj = tmp2;
+                    DicTmp.Remove(id);
+                    WeakCount++;
+                    return true;
+                }
+            }
+            else
+            {
+                Strong.Remove(id);
+                obj = tmp;
+                StrongCount++;
+                return true;
+            }
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp45/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashManager cm = new CashManager(5, 5);
+            int num = 0;
+            string obj = "sent";
+            do
+            {
+                obj = obj + num;
+                num++;
+
+
+            } while (cm.Add(num, obj));
+
+            num = 0;
+            object obj2 = new object();
+            do
+            {
+                GC.Collect();
+                num++;
+                if (obj2 != null) Console.WriteLine(obj2.ToString());
+
+
+            } while (cm.GetAndRemove(num, out obj2));
+
+        }
+    }
+}
